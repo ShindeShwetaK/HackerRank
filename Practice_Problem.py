1dd2ee9881c69f4e7bd757abf928db925aaa7a37
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-07-08 11:02:06 -0700

    Update Practice_Problem.py

--- Practice_Problem.py
+++ Practice_Problem.py
@@ -217,7 +217,22 @@ or
         i += 1
     return count
 ##########################################################################
-Q12.
+#Q12.XOR string
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0';
+        else:
+            res += '1';
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
+##########################################################################
+
 
                
 
