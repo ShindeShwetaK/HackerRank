ec0ee5c9e03e0adb316eefa53ff652aad1dfb663
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-07-09 12:28:53 -0700

    Create Practice_Problems_Week2.py

---
+++ Practice_Problems_Week2.py
@@ -0,0 +1,23 @@
+#####################################Week2#############################
+#https://www.hackerrank.com/interview/preparation-kits/one-month-preparation-kit/one-month-week-two/challenges
+######################################################################
+#Q1.Sale by match.
+from collections import Counter
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs=Counter(ar)
+    count_pairs=0
+    for i in pairs:
+        count_pairs+=pairs.get(i)//2
+    return count_pairs
+  from collections import Counter
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs=Counter(ar)
+    count_pairs=0
+    for i in pairs:
+        count_pairs+=pairs.get(i)//2
+    return count_pairs
+  #Python Counter:https://www.youtube.com/watch?v=Lmsz6h25yiA
+######################################################################
+#Q2.
